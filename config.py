11f0f5402a23c3f294771c64b5c74c375ccf0130
unknown <pydev789@protonmail.com>
2020-04-10 18:26:08 +0300

    initail server setup

---
+++ config.py
@@ -0,0 +1,5 @@
+# config.py
+# Purpose: Configuration constants for the flask app
+
+SQLALCHEMY_DATABASE_URI = "postgresql://localhost/monsters"
+SQLALCHEMY_TRACK_MODIFICATIONS = False
---
+++ setup.py
@@ -0,0 +1,8 @@
+# setup.py
+# Purpose: Using the context of the app, initialize the database tables
+
+from app import app
+from models import db
+
+with app.app_context():
+    db.create_all()
